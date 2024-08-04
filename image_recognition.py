b5a946693a6ea485aadeaeee7394e197ff013cb2
Kevin <136887074+Kevinnnnb@users.noreply.github.com>
2024-08-04 23:39:27 +0200

    Fix the tesseract bug

--- image_recognition.py
+++ image_recognition.py
@@ -1,16 +1,13 @@
-from PIL import Image
 import pytesseract
+from PIL import Image
+
+pytesseract.pytesseract.tesseract_cmd = '/opt/homebrew/bin/tesseract' # Replace with the path to your Tesseract executable (execute this to get the path : 'which tesseract')
 
 def ocr_image(image_path):
     img = Image.open(image_path)
-    
     text = pytesseract.image_to_string(img)
-    
-    print("Texte extrait de l'image :\n\n")
     print(text)
 
-
-
-image_path = 'Your path to the image file here'
-
+# Example usage
+image_path = '' #Replace with your image file path
 ocr_image(image_path)
