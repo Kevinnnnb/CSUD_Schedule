e1391e2520edc242ada3e768c868d51ca71aa0f6
Kevin <136887074+Kevinnnnb@users.noreply.github.com>
2024-08-06 09:24:44 +0200

    Update isa.py

--- ISA/isa.py
+++ ISA/isa.py
@@ -12,44 +12,38 @@ driver = webdriver.Safari()
 print('Executing script...\n')
 
 try:
-    # Open the login page
     url = "https://isa.fr.ch"
     driver.get(url)
     time.sleep(5) 
     
     driver.find_element(By.NAME, "loginfmt").send_keys(username) 
-    time.sleep(5)
     driver.find_element(By.ID, "idSIButton9").send_keys(Keys.RETURN)
     time.sleep(5)
     driver.find_element(By.NAME, "passwd").send_keys(password)
-    time.sleep(5)
     driver.find_element(By.ID, "idSIButton9").send_keys(Keys.RETURN)
     time.sleep(5)
 
-    # trying to click on the sms authenticator
     image_element = driver.find_element(By.CLASS_NAME, "tile-img")
     image_element.click()
     time.sleep(5)
     
     verification_code = input("Enter the SMS verification code you received: ")
     
-    # Enter the verification code into the appropriate field
-    driver.find_element(By.ID, "idTxtBx_SAOTCC_OTC").send_keys(verification_code)  # Update with the actual name attribute
-    time.sleep(5)
+    driver.find_element(By.ID, "idTxtBx_SAOTCC_OTC").send_keys(verification_code) 
     driver.find_element(By.ID, "idSubmit_SAOTCC_Continue").send_keys(Keys.RETURN)
-    time.sleep(5) 
+    time.sleep(20) 
     driver.find_element(By.ID, "idSIButton9").send_keys(Keys.RETURN)
-    time.sleep(0.5)
+    time.sleep(5)
+
+    print('ACCESS GRANTED !\n')
 
 
     driver.switch_to.window(driver.window_handles[-1])
-    # Open a new URL in the new tab
     new_tab_url = "https://appls.edufr.ch/isaweb/!PORTAL17S.portalCell?ww_k_cell=456253168&zz_b_firstloading=1&ww_n_cellkey=696656199&ww_n_ctrlKey=330989937"
     driver.get(new_tab_url)
     time.sleep(5) 
 
     page_source = driver.page_source
-
     xml_content = page_source
 
     print('printing page content...\n')
@@ -58,4 +52,7 @@ try:
 
 finally:
     driver.quit()
-    print('Done Successfully !\n')
+    if xml_content is not None:
+        print('Done Successfully !\n')
+    else:
+        print('Something bad happend ...\n')
